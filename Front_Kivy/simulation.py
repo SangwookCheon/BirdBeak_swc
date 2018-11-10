8a099d4271905dc0306bcab08bdaac2645ce4f4a
sangwookcheon <sangwookchn@gmail.com>
2018-11-10 21:25:06 +0700

    added android apk implementation

--- Front_Kivy/simulation.py
+++ Front_Kivy/simulation.py
@@ -3,14 +3,16 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
 
 class Simulation(FloatLayout):
     def __init__(self):
         super(Simulation, self).__init__()
+        self.scatter = Scatter()
         self.b1 = Button(text = "Button 1")
-        self.label1 = Label(text = print_message(), pos = (100,10))
-        self.add_widget(self.b1)
-        self.add_widget(self.label1)
+        self.label1 = Label(text = print_message())
+        self.add_widget(self.scatter)
+        self.scatter.add_widget(self.label1)
 
 class MyApp(App):
     def build(self):
